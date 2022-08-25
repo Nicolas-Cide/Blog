5bb1798f165ebcdddde21d8dd6d4cea8ed8e52ae
Nicolas-Cide <nicocide351@gmail.com>
2022-08-25 09:15:39 -0300

    Cambios panel administrador

--- Blog/appFamily/admin.py
+++ Blog/appFamily/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
-
+from appFamily.models import Clientes,Productos,Pedidos
 # Register your models here.
+
+class ClientesAdmin(admin.ModelAdmin):
+    list_display = ("name", "adress", "email", "phone", "comment")
+    search_fields = ("name", "adress")
+    list_filter = ("adress",)
+
+class ProductosAdmin(admin.ModelAdmin):
+    list_display = ("name", "section", "price")
+    search_fields = ("name", "section")
+    list_filter = ("section",)
+
+class PedidosAdmin(admin.ModelAdmin):
+    list_display = ("number", "date", "delivered")
+    search_fields = ("number", "date")
+    list_filter = ("delivered","date")
+    date_hierarchy = "date"
+
+admin.site.register(Clientes,ClientesAdmin)
+admin.site.register(Productos,ProductosAdmin)
+admin.site.register(Pedidos,PedidosAdmin)
--- Blog/appFamily/models.py
+++ Blog/appFamily/models.py
@@ -5,11 +5,12 @@ from django.db import models
 class Clientes(models.Model):
     name = models.CharField(max_length=30)
     adress = models.CharField(max_length=30)
-    email = models.EmailField()
+    email = models.EmailField(verbose_name="Email o Gmail")
     phone = models.IntegerField()
+    comment = models.CharField(max_length=30,blank=True,null=True)
 
-    def __str__(self):
-        return f'Cliente: {self.name}, Direc: {self.adress}, email: {self.email}, phone: {self.phone}'
+    #def __str__(self):
+     #   return f'Cliente: {self.name} | Direc: {self.adress} | email: {self.email} | Phone: {self.phone} | Comentario: {self.comment}'
 
 
 class Productos(models.Model):
@@ -17,8 +18,15 @@ class Productos(models.Model):
     section = models.CharField(max_length=20)
     price = models.IntegerField()
 
+    #def __str__(self):
+     #   return f"Producto: {self.name} | Sección: {self.section} | Precio: {self.price}"
 
 class Pedidos(models.Model):
     number = models.IntegerField()
     date = models.DateField()
     delivered = models.BooleanField()
+
+    #def __str__(self):
+     #   return f"Numero: {self.number} | Fecha: {self.date} | Entregado: {self.delivered}"
+
+
