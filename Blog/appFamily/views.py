1d20fe6cf059bda32dd490e02ce309b922bb0df2
Nicolas-Cide <nicocide351@gmail.com>
2022-08-25 20:03:22 -0300

    Se agregan forms

--- Blog/appFamily/views.py
+++ Blog/appFamily/views.py
@@ -1,4 +1,15 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
 
 # Create your views here.
 
+def search_products(request):
+
+    return render(request, "searchProducts.html")
+
+
+#def search(request):
+
+    msg = "Articulo buscado: %r" %request.GET["product"]
+
+    return HTTPResponse(msg)
